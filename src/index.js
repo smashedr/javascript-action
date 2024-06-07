3828d7b3f289bed7c1e4c6b1d4dfd2bef6e10c18
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-07 00:41:19 -0700

    Test

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ const semver = require('semver')
         if (!github.context.ref.startsWith('refs/tags/')) {
             return core.info(`Skipping due to no tag: ${github.context.ref}`)
         }
-        console.log('ref:', github.context.ref)
-        const parsedTag = github.context.ref.replace('refs/tags/', '')
-        console.log('parsedTag:', parsedTag)
-        const version = semver.clean(parsedTag)
-        console.log('semver:', version)
+        // console.log('ref:', github.context.ref)
+        // const parsedTag = github.context.ref.replace('refs/tags/', '')
+        // console.log('parsedTag:', parsedTag)
+        // const version = semver.clean(parsedTag)
+        // console.log('semver:', version)
 
         // Process Inputs
         const inputFile = core.getInput('file')
         console.log('file:', inputFile)
         const inputVersion = core.getInput('version')
         console.log('version:', inputVersion)
+        console.log('version:', typeof inputVersion)
+        const version = semver.clean(inputVersion)
+        console.log('semver:', version)
 
         // // Set Variables
         // const { owner, repo } = github.context.repo
